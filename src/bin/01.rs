bfe434ab045cd24291b3066ae3094bc86fd1e3a3
FabiSahne <hfranz90@gmail.com>
2024-12-01 13:02:57 +0100

    Day 01 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,7 +3,7 @@ use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -52,17 +52,39 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<i32> {
+        let (mut left, right) =
+            reader
+                .lines()
+                .fold((vec![], vec![]), |(mut left, mut right), line| {
+                    let line = line.unwrap();
+                    let line = line.split_whitespace().collect::<Vec<&str>>();
+                    left.push(line[0].parse::<i32>().unwrap());
+                    right.push(line[1].parse::<i32>().unwrap());
+                    (left, right)
+                });
+
+        let mut freq_table = HashMap::new();
+        for n in right {
+            freq_table.entry(n).and_modify(|e| *e += 1).or_insert(1);
+        }
+
+        for n in left.iter_mut() {
+            *n *= freq_table.get(n).unwrap_or(&0);
+        }
+
+        let answer = left.into_iter().sum::<i32>();
+
+        Ok(answer)
+    }
+
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
