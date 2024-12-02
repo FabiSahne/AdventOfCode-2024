df03ceef5195c9272fbd5bba8fceb82008e523d2
FabiSahne <hfranz90@gmail.com>
2024-12-02 12:53:29 +0100

    Day 02 Part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::wildcard_imports)]
+#![allow(clippy::items_after_statements)]
 use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
@@ -6,7 +7,7 @@ use const_format::concatcp;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const DAY: &str = "02"; // TODO: Fill the day
+const DAY: &str = "02";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
@@ -16,18 +17,44 @@ const TEST: &str = "\
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
-"; // TODO: Add the test input
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
 
-    //region Part 1
     println!("=== Part 1 ===");
 
-    #[allow(clippy::items_after_statements)]
+    //region Part 1
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let answer = reader.lines().map_while(Result::ok).count();
+        let mut reports = vec![];
+        for rline in reader.lines() {
+            let line = rline?;
+            reports.push(
+                line.split_whitespace()
+                    .map(|s| s.parse::<i32>().unwrap_or(0))
+                    .collect::<Vec<i32>>(),
+            );
+        }
+
+        let answer = reports.into_iter().filter(|r| is_safe(r)).count();
+
         Ok(answer)
+    }
+    fn is_safe(report: &[i32]) -> bool {
+        assert!(report.len() >= 2);
+        if report[1] > report[0] && report[1] <= report[0] + 3 {
+            return is_ascending(report);
+        }
+        if report[0] > report[1] && report[0] <= report[1] + 3 {
+            return is_descending(report);
+        }
+        false
+    }
+    fn is_ascending(report: &[i32]) -> bool {
+        report.windows(2).all(|w| w[1] > w[0] && w[1] <= w[0] + 3)
+    }
+    fn is_descending(report: &[i32]) -> bool {
+        report.windows(2).all(|w| w[0] > w[1] && w[0] <= w[1] + 3)
     }
 
     assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
