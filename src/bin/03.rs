068437dc8263da1a76f19e0c2cc200ebcad7cd9e
FabiSahne <hfranz90@gmail.com>
2024-12-03 15:40:03 +0100

    Day 03

---
+++ src/bin/03.rs
@@ -0,0 +1,75 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "03";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+";
+
+fn calc(text: &str) -> Result<usize> {
+    let re = Regex::new(r"mul\((?<m1>[0-9]+),(?<m2>[0-9]+)\)")?;
+    Ok(re
+        .captures_iter(text)
+        .map(|caps| {
+            let m1 = caps.name("m1").unwrap().as_str().parse::<usize>().unwrap();
+            let m2 = caps.name("m2").unwrap().as_str().parse::<usize>().unwrap();
+            m1 * m2
+        })
+        .sum())
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
+        let mut text = String::new();
+        reader.read_to_string(&mut text)?;
+        calc(text.as_str())
+    }
+
+    assert_eq!(161, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {result}");
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(mut reader: R) -> Result<usize> {
+        let mut text = String::new();
+        reader.read_to_string(&mut text)?;
+        while let Some(dont) = text.find("don't()") {
+            if let Some(do_) = text.find("do()") {
+                if do_ < dont {
+                    text.replace_range(do_..(do_ + 4), "..");
+                } else {
+                    text.replace_range(dont..(do_ + 4), "");
+                }
+            } else {
+                text.replace_range(dont.., "");
+            }
+        }
+        calc(text.as_str())
+    }
+
+    assert_eq!(48, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {result}");
+    //endregion
+
+    Ok(())
+}
