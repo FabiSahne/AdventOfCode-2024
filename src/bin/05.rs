9d04ef2cdacaf8087d6129844b0580b057a60082
FabiSahne <hfranz90@gmail.com>
2024-12-05 12:23:04 +0100

    Day 05 - Part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,6 +2,7 @@ use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -38,6 +39,46 @@ const TEST: &str = "\
 61,13,29
 97,13,75,29,47
 ";
+type Data = (Vec<Vec<usize>>, Vec<Vec<usize>>);
+
+fn read_data<R: BufRead>(reader: R) -> Result<Data> {
+    let mut ordering = vec![];
+    let mut updates = vec![];
+    let mut read_ordering = false;
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.is_empty() {
+            read_ordering = true;
+            continue;
+        }
+        if read_ordering {
+            updates.push(
+                line.split(',')
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>(),
+            );
+        } else {
+            ordering.push(
+                line.split('|')
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>(),
+            );
+        }
+    }
+
+    Ok((ordering, updates))
+}
+
+fn fix_order(a: &usize, b: &usize, ordering: &[Vec<usize>]) -> Ordering {
+    if ordering.contains(&vec![*a, *b]) {
+        return Ordering::Less;
+    }
+    if ordering.contains(&vec![*b, *a]) {
+        return Ordering::Greater;
+    }
+    Ordering::Equal
+}
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -45,42 +86,8 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
-        let mut ordering = vec![];
-        let mut updates = vec![];
-        let mut read_ordering = false;
-
-        // let mut buf = String::new();
-        // for _ in 0.. {
-        //     buf.clear();
-        //     reader.read_line(&mut buf)?;
-        //     if buf.is_empty() {
-        //         break;
-        //     } else {
-        //         ordering.push(buf.split('|').map(|s| s.parse::<usize>().unwrap()).collect::<Vec<_>>());
-        //     }
-        // }
-
-        for line in reader.lines() {
-            let line = line?;
-            if line.is_empty() {
-                read_ordering = true;
-                continue;
-            }
-            if read_ordering {
-                updates.push(
-                    line.split(',')
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .collect::<Vec<_>>(),
-                );
-            } else {
-                ordering.push(
-                    line.split('|')
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .collect::<Vec<_>>(),
-                );
-            }
-        }
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let (ordering, updates) = read_data(reader)?;
 
         let mut answer = 0;
 
@@ -112,17 +119,38 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {result}");
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let (ordering, mut updates) = read_data(reader)?;
+
+        let mut answer = 0;
+
+        for update in &mut updates {
+            let mut order_was_incorrect = false;
+            for order in &ordering {
+                if let Some(first) = update.iter().position(|&item| item == order[0]) {
+                    if let Some(second) = update.iter().position(|&item| item == order[1]) {
+                        if first > second {
+                            order_was_incorrect = true;
+                        }
+                    }
+                }
+            }
+            if order_was_incorrect {
+                update.sort_unstable_by(|a, b| fix_order(a, b, &ordering));
+                answer += update[update.len() / 2];
+            }
+        }
+
+        Ok(answer)
+    }
+
+    assert_eq!(123, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {result}");
     //endregion
 
     Ok(())
