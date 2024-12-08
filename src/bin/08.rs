66c43cec934d4f318fb36263d47b2476f92bf2f4
FabiSahne <hfranz90@gmail.com>
2024-12-08 13:36:23 +0100

    Day 08 - Part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -25,15 +25,15 @@ const TEST: &str = "\
 ............
 ";
 
+fn is_valid_antinode(node: (i32, i32), max_x: usize, max_y: usize) -> bool {
+    node.0 >= 0 && node.1 >= 0 && (node.0 as usize) < max_x && (node.1 as usize) < max_y
+}
 fn get_antinodes(
     node1: (usize, usize),
     node2: (usize, usize),
     max_x: usize,
     max_y: usize,
 ) -> Vec<(usize, usize)> {
-    fn is_valid_antinode(node: (i32, i32), max_x: usize, max_y: usize) -> bool {
-        node.0 >= 0 && node.1 >= 0 && (node.0 as usize) < max_x && (node.1 as usize) < max_y
-    }
     let (node1_x, node1_y) = (node1.0 as i32, node1.1 as i32);
     let (node2_x, node2_y) = (node2.0 as i32, node2.1 as i32);
 
@@ -54,6 +54,46 @@ fn get_antinodes(
     nodes
 }
 
+fn get_antinodes_part2(
+    node1: (usize, usize),
+    node2: (usize, usize),
+    max_x: usize,
+    max_y: usize,
+) -> Vec<(usize, usize)> {
+    let (node1_x, node1_y) = (node1.0 as i32, node1.1 as i32);
+    let (node2_x, node2_y) = (node2.0 as i32, node2.1 as i32);
+
+    let mut nodes = vec![];
+
+    let x_diff = node2_x - node1_x;
+    let y_diff = node2_y - node1_y;
+
+    let mut antinode1 = (node1_x - x_diff, node1_y - y_diff);
+    let mut antinode2 = (node2_x + x_diff, node2_y + y_diff);
+
+    while is_valid_antinode(antinode1, max_x, max_y) {
+        nodes.push((antinode1.0 as usize, antinode1.1 as usize));
+        antinode1 = (antinode1.0 - x_diff, antinode1.1 - y_diff);
+    }
+    while is_valid_antinode(antinode2, max_x, max_y) {
+        nodes.push((antinode2.0 as usize, antinode2.1 as usize));
+        antinode2 = (antinode2.0 + x_diff, antinode2.1 + y_diff);
+    }
+    nodes
+}
+
+fn get_map(data: &[Vec<char>]) -> HashMap<char, Vec<(usize, usize)>> {
+    let mut node_map = HashMap::new(); // (x, y) coords for every type of node
+    for (y, line) in data.iter().enumerate() {
+        for (x, &ch) in line.iter().enumerate() {
+            if ch.is_alphanumeric() {
+                node_map.entry(ch).or_insert_with(Vec::new).push((x, y));
+            }
+        }
+    }
+    node_map
+}
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -62,14 +102,7 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let mut data = read_lines_to_vec_vec_char(reader);
-        let mut node_map = HashMap::new(); // (x, y) coords for every type of node
-        for (y, line) in data.iter().enumerate() {
-            for (x, &ch) in line.iter().enumerate() {
-                if ch.is_alphanumeric() {
-                    node_map.entry(ch).or_insert_with(Vec::new).push((x, y));
-                }
-            }
-        }
+        let node_map = get_map(&data);
 
         let (x, y) = (data[0].len(), data.len());
         let mut answer = 0;
@@ -99,17 +132,38 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {result}");
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let mut data = read_lines_to_vec_vec_char(reader);
+        let node_map = get_map(&data);
+
+        let (x, y) = (data[0].len(), data.len());
+
+        for (_, coords) in node_map {
+            for pair in coords.iter().combinations(2) {
+                let node1 = *pair[0];
+                data[node1.1][node1.0] = '#';
+                let node2 = *pair[1];
+                data[node2.1][node2.0] = '#';
+
+                for antinode in get_antinodes_part2(node1, node2, x, y) {
+                    data[antinode.1][antinode.0] = '#'
+                }
+            }
+        }
+
+        Ok(data
+            .into_iter()
+            .map(|row| row.into_iter().filter(|&ch| ch == '#').count())
+            .sum())
+    }
+
+    assert_eq!(34, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {result}");
     //endregion
 
     Ok(())
